95a5de5cd8d1777720c5eba3c4bd38b077088778
Tyler Hancock <darklime@live.ca>
2025-10-16 16:32:50 -0600

    Update build dependencies.

--- forge/src/main/java/net/darkhax/nyctography/forge/ForgeMod.java
+++
@@ -1,8 +0,0 @@
-package net.darkhax.nyctography.forge;
-
-import net.darkhax.nyctography.common.impl.NyctographyMod;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(NyctographyMod.MOD_ID)
-public class ForgeMod {
-}
