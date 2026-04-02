3ca18a4f399c8343ad2c81ef0d1267fad75c2fa5
Tyler Hancock <darklime@live.ca>
2026-04-02 16:02:30 -0600

    Fix crash when the config is accessed in a async context, like recipe loading.

--- common/src/main/java/net/darkhax/nyctography/common/impl/Content.java
+++ common/src/main/java/net/darkhax/nyctography/common/impl/Content.java
@@ -24,7 +24,7 @@ public class Content implements ContentProvider {
 
     @Override
     public void defineTrades(VillagerTradeAdapter registry) {
-        final TradeConfig config = NyctographyMod.CONFIG.get().banner_stencil_trade;
+        final TradeConfig config = NyctographyMod.CONFIG.banner_stencil_trade;
         if (config.enabled) {
             registry.addWanderingTrade(new VillagerOffers(config.tradeOffer), config.is_rare);
         }
--- common/src/main/java/net/darkhax/nyctography/common/impl/NyctographyMod.java
+++ common/src/main/java/net/darkhax/nyctography/common/impl/NyctographyMod.java
@@ -16,11 +16,11 @@ public class NyctographyMod {
     public static final String MOD_ID = "nyctography";
     public static final String MOD_NAME = "Nyctography";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
-    public static final CachedSupplier<Config> CONFIG = CachedSupplier.cache(() -> ConfigManager.load(MOD_ID, new Config()));
+    public static final Config CONFIG = ConfigManager.load(MOD_ID, new Config());
 
     public static final CachedSupplier<Map<ResourceLocation, Supplier<Boolean>>> PROPERTIES = CachedSupplier.cache(() -> {
         final Map<ResourceLocation, Supplier<Boolean>> properties = new HashMap<>();
-        properties.put(id("craftable_banner_pattern"), () -> CONFIG.get().craftable_banner_pattern);
+        properties.put(id("craftable_banner_pattern"), () -> CONFIG.craftable_banner_pattern);
         return properties;
     });
 
